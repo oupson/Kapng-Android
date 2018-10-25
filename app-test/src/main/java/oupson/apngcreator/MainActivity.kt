4ad6d7ca61933aef30fee895ffb2ce4932b78b1c
oupson <oupson1er@gmail.com>
2018-10-25 14:51:56 +0200

    Optimize drawing

--- app-test/src/main/java/oupson/apngcreator/MainActivity.kt
+++ app-test/src/main/java/oupson/apngcreator/MainActivity.kt
@@ -1,38 +1,17 @@
 package oupson.apngcreator
 
-import android.graphics.BitmapFactory
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.AnimationDrawable
-import android.os.Environment
-import android.os.Environment.DIRECTORY_PICTURES
-import android.system.Os
-import oupson.apng.APNGDisassembler
-import java.io.File
-import android.os.Environment.getExternalStorageDirectory
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.doAsync
-import oupson.apng.ApngAnimator
-import java.nio.file.Files.exists
-import android.system.Os.mkdir
-import android.util.Log
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
 import com.squareup.picasso.Picasso
-import oupson.apng.Apng
-import oupson.apng.Utils
-import java.io.ByteArrayOutputStream
-import java.lang.reflect.Field
-import java.net.HttpURLConnection
-import java.net.HttpURLConnection.HTTP_OK
-import java.net.URL
+import kotlinx.android.synthetic.main.activity_main.*
+import oupson.apng.ApngAnimator
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var animator : ApngAnimator
 
     val imageUrl = "https://metagif.files.wordpress.com/2015/01/bugbuckbunny.png"
-
+    //val imageUrl = "https://raw.githubusercontent.com/tinify/iMessage-Panda-sticker/master/StickerPackExtension/Stickers.xcstickers/Sticker%20Pack.stickerpack/panda.sticker/panda.png"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
